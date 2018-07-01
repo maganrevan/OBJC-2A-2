f7498da570b560c3aeef925bac09eeeba94f9126
Magnus Kruschwitz <magnus.kruschwitz@outlook.com>
2018-07-01 20:52:11 +0200

    Aufgabe 2. OBJC 2A-XX1-N01

--- OBJC-2A-2/main.swift
+++ OBJC-2A-2/main.swift
@@ -1,3 +1,4 @@
+/* #################################################### Einsendeaufgabe 2.2 #################################################### */
 //
 //  main.swift
 //  OBJC-2A-2
@@ -5,8 +6,14 @@
 //  Created by Magnus Kruschwitz on 01.07.18.
 //  Copyright © 2018 Magnus Kruschwitz. All rights reserved.
 //
+//  Zur Erklärung, das i steht hier für den Typen der Variable, in dem Fall Integer, wenn z.B. die eingelesene Zahl als Double gekennzeichent werden würde, dann würde dort ein d vor dem Bezeichner stehen, o für Objekte, a für Array, ggf. auch ai für Array of Integer, hat sich bislang in meinem Betrieb so ergeben und es gab keinerlei Probleme mit diesem Vorgehen.
 
 import Foundation
 
-print("Hello, World!")
+print("Bitte geben Sie eine Ganzzahl ein.")
+var iNumber1 = Int(readLine()!)!
 
+print("Bitte geben Sie eine weitere Ganzzahl ein.")
+var iNumber2 = Int(readLine()!)!
+
+print("Das Ergebnis der Division von \(iNumber1) durch \(iNumber2) lautet \(Double(iNumber1) / Double(iNumber2))")
